35a186032b3aebb9eca804264c2452a6ca701151
Jordi Gonzalez <jordigonzalez123@gmail.com>
2022-07-14 15:33:38 +0200

    Intento de SearchBar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
- 
+import React from 'react';
 import './App.css';
-import "./Bootstrap.css"
+import "./Bootstrap.css";
 import { Routes, Route, Link } from "react-router-dom";
-import MainPage from "./Vistas/MainPage"
-import Login from "./Vistas/Login"
-import Cart from "./Vistas/Cart"
+import SearchBar from "./Components/SearchBar/SearchBar";
 
-function App() {
+
+function Sear() {
   return (
     <div className="App">
-      app
-      {/* <Cart></Cart> */}
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="about" element={<Login />} />
-      </Routes> */}
+      <SearchBar placeholder="Buscar Productos" />
     </div>
   );
 }
+
 
 export default App;
