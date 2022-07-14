0db11de4409997adf96cb5b772e07ec61dfbcc01
matt sb <mattsandovalb@gmail.com>
2022-07-14 09:36:50 +0200

    product-detail-commit

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,18 +1,37 @@
-import React, {useEffect} from 'react'
-
-
-
+import React from "react";
+import "./ProductDetails.css";
+import img1 from "../../img/salmorejo.jpg"
 
 
 
 function ProductDetails() {
-
   return (
-    <div>ProductDetails</div>
-  )
+    <div>
+      <div className="container-sm">
+        <div className="">
+          <p className="">Conservas, caldos y cremas-Gazpacho y cremas</p>
+          <img className="rounded" src={img1} alt=""></img>
+        </div>
+        <div className=" container-sm">
+          <h2>Salmorejo al estilo cordobés Hacendado</h2>
+          <p>Brick 1 L | 1,70 €/L</p>
+          <h3>1,70 €/ud.</h3>
+          <hr className=""></hr>
+          <div className="">
+            <div className="container-lg ">
+              <input className="w-25 m-4 rounded" type='number' placeholder={1}></input>
+            </div>
+            <div className="container-lg ">
+            <button className="btn-lg bg-danger text-color-white border-0 rounded-pill shadow-lg p-3 mb-5 rounded-pill ">AÑADIR AL CARRO</button>
+            </div>
+          </div>
+        </div>
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-export default ProductDetails
+export default ProductDetails;
