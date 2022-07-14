908e742e7972146cc5b403bc728623afe50b3262
AlessHub <aless312@gmail.com>
2022-07-14 13:54:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
  
 import './App.css';
-import "./Bootstrap.css"
-import { Routes, Route, Link } from "react-router-dom";
-import MainPage from "./Vistas/MainPage"
-import Login from "./Vistas/Login"
-import Cart from "./Vistas/Cart"
+import "./Bootstrap.css";
+import Category from './components/Category/Category';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 function App() {
   return (
     <div className="App">
-      app
-      {/* <Cart></Cart> */}
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="about" element={<Login />} />
-      </Routes> */}
+      <Category />
     </div>
   );
 }
