ad637f56f269d02e9a9538a72923d3eb6f75c512
BilalBawar <106524077+BilalBawar@users.noreply.github.com>
2022-07-14 10:46:15 +0200

    esto mio fotter de bilal

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import "./Bootstrap.css"
 import { Routes, Route, Link } from "react-router-dom";
 import MainPage from "./Vistas/MainPage"
 import Login from "./Vistas/Login"
-import Cart from "./Vistas/Cart"
+import Foter from "./components/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
-      app
-      {/* <Cart></Cart> */}
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="about" element={<Login />} />
-      </Routes> */}
+      <Foter></Foter>
+      
     </div>
   );
 }
