f8b2a5f3e7b9b2c2cbe3c80e13af98f9ff8ffd68
Alejandra MCuitino <ale.morales11@gmail.com>
2022-07-14 13:54:08 +0200

    'intento de navbar'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
  
 import './App.css';
-import "./Bootstrap.css"
-import { Routes, Route, Link } from "react-router-dom";
-import MainPage from "./Vistas/MainPage"
-import Login from "./Vistas/Login"
-import Cart from "./Vistas/Cart"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Bootstrap.css";
+import { Link, BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      app
-      {/* <Cart></Cart> */}
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="about" element={<Login />} />
-      </Routes> */}
+      <Navbar/>
     </div>
   );
 }
