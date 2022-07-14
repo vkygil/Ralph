d372c3f0f7cc75983d23306b6c5a7e143cd9ef7a
Hudu <ahmedhudu21@gmail.com>
2022-07-14 12:00:29 +0200

    working login with validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import Image from '../../images/logo.png';
 import { FaGoogle } from 'react-icons/fa';
+import useLogin from './useLogin';
 
 
 const Login = () => {
+
+  const { handleChange, values, handleSubmit } = useLogin();
+
+
   return (
     <>
       <img src={Image} alt="logo" />
       <h1>Identificate</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h3>Correo electronico</h3>
         <div className="form-input">
           <label className="form-label">
-            <input type="text" id="email" name="email" className="label-input" placeholder="Ejemplo@gmail.com"></input>
+            <input type="text" id="email" name="email" className="label-input" placeholder="Ejemplo@gmail.com"
+              value={values.email} onChange={handleChange}></input>
           </label>
         </div>
         <h3>Contraseña</h3>
         <div className="form-input">
           <label className="form-label">
-            <input type="text" id="password" name="password" className="label-input" placeholder="xxxxxxxx"></input>
+            <input type="password" name="password" className="label-input" placeholder="xxxxxxxx"
+              value={values.password} onChange={handleChange}></input>
           </label>
         </div>
         <div className="signUp-btn">
