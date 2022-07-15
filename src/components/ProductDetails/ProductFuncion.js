a3687d0b1d2c7e03641b1400c68823bf1cd5aacc
matt sb <mattsandovalb@gmail.com>
2022-07-15 10:23:27 +0200

    responsive-finish-commit

--- src/components/ProductDetails/ProductFuncion.js
+++ src/components/ProductDetails/ProductFuncion.js
@@ -1,2 +1,28 @@
+import React, { useEffect } from "react";
 
 
+const ProductFuncion = () =>{
+
+    const [products, name] = React.userState(null);
+
+    React.useEffect(() => {
+        // console.log('useEffect')
+        GetProduct();
+    },[])
+
+    const GetProduct = async() =>{
+       const data = await fetch('https://api.kroger.com/v1 /productos');
+       const products = await data.json();
+       products(name);
+    }
+
+    return(
+        <div>
+
+            <h1></h1>
+
+        </div>
+    )
+}
+
+export default ProductFuncion
