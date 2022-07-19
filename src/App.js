3a519a3c96956eb12db3a9230a3d5e607c62dfbc
Jordi Gonzalez <jordigonzalez123@gmail.com>
2022-07-19 10:48:00 +0200

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+
 import './App.css';
 import "./Bootstrap.css";
+import Category from './components/Category/Category';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "./Bootstrap.css"
 import { Routes, Route, Link } from "react-router-dom";
-import SearchBar from "./Components/SearchBar/SearchBar";
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import MainPage from "./Vistas/MainPage";
+import Login from './components/Login/Login';
+import ProductList from './components/ProductList/ProductList';
+import Navbar from './components/Navbar/Navbar'
 
 
-function Sear() {
+function App() {
   return (
     <div className="App">
-      <SearchBar placeholder="Buscar Productos" />
+      <Navbar />
+      <Category />
+
+      <div className='container'>
+        <br /><hr />
+        <h1>Product list:</h1>
+        <ProductList />
+      </div>
+      <br /><hr />
+
+      <ProductDetails />
+      <Login />
+      
+      {/* <Routes>
+      <Login></Login>
+      {/* <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="about" element={<Login />} />
+      </Routes>  */}
     </div>
   );
 }
-
 
 export default App;
