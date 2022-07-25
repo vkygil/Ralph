ba63ff20a988196f4c69b5a96259a56debcdf61a
Alejandra MCuitino <ale.morales11@gmail.com>
2022-07-25 10:01:35 +0200

    Router semidone

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import "./Bootstrap.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 //import components and views
-import Product from './Vistas/Product';
-import Login from './Vistas/Login';
-import Cart from './Vistas/Cart';
 import MainPage from './Vistas/MainPage';
-import Navbar from './components/Navbar/Navbar';
-import CategoryList from './components/CategoryList/CategoryList';
+
 
 function App() {
   return (
--- src/Vistas/MainPage.jsx
+++ src/Vistas/MainPage.jsx
@@ -3,10 +3,10 @@
 // Categorias List
 // Footer
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
-import CategoryList from '../components/CategoryList/CategoryList'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
+import  Navbar  from '../components/Navbar/Navbar'
+import  CategoryList  from '../components/CategoryList/CategoryList'
+import  Header  from '../components/Header/Header'
+import  Footer from '../components/Footer/Footer'
 
 function MainPage() {
   return (
