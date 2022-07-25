cdf169b2b0fc807d65f2bdd489ffe3a7c02d8f70
Alejandra MCuitino <ale.morales11@gmail.com>
2022-07-25 11:51:49 +0200

    correction errors

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 
 import './App.css';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Bootstrap.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 //import components and views
 import MainPage from './Vistas/MainPage';
-=======
-import "./Bootstrap.css";
-import Category from './components/Category/Category';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-// import "./Bootstrap.css"
-import { Routes, Route, Link } from "react-router-dom";
-import ProductDetails from './components/ProductDetails/ProductDetails';
-import MainPage from "./Vistas/MainPage";
-import Login from './components/Login/Login';
->>>>>>> testBranch
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
          
          <MainPage/>
      
-=======
-      <Category />
-      <ProductDetails/>
-      <Login />
-      {/* <Routes>
-      <Login></Login>
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="about" element={<Login />} />
-      </Routes>  */}
->>>>>>> testBranch
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
 import Router from './Application/Router';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min"; 
-=======
-import App from './App'; 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
->>>>>>> testBranch
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
