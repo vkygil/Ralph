58ce927d9429ca2b3aab8f489bbd017697678289
BilalBawar <106524077+BilalBawar@users.noreply.github.com>
2022-07-25 12:25:28 +0200

    Formulario

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { Routes, Route, Link } from "react-router-dom";
 import MainPage from "./Vistas/MainPage"
 import Login from "./Vistas/Login"
 import Foter from "./components/Footer/Footer"
+import Formolario from "./components/Formolario/Formolario"
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Foter></Foter>
+      <Formolario></Formolario>
+     
+     
       
     </div>
   );
